819b5f02d4cec8aaaa41722f37fb16eaf2570630
Matthew Faltys <mfaltys@gmail.com>
2017-10-09 11:03:52 -0500

    Create initial server and return rcode3 to all

---
+++ blackhole.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+
+	"github.com/miekg/dns"
+	"github.com/unixvoid/glogger"
+	"gopkg.in/gcfg.v1"
+	"gopkg.in/redis.v5"
+)
+
+type Config struct {
+	Blackhole struct {
+		Loglevel string
+		DNSPort  int
+	}
+	Redis struct {
+		Host     string
+		Password string
+	}
+}
+
+var (
+	config = Config{}
+)
+
+func main() {
+	readConf()
+	initLogger(config.Blackhole.Loglevel)
+	redisClient, redisErr := initRedisConnection()
+	if redisErr != nil {
+		glogger.Error.Println("redis connection cannot be made.")
+		glogger.Error.Println("cryodns will continue to function in passthrough mode only")
+	} else {
+		glogger.Debug.Println("connection to redis succeeded.")
+	}
+
+	// format the string to be :port
+	fPort := fmt.Sprint(":", config.Blackhole.DNSPort)
+
+	udpServer := &dns.Server{Addr: fPort, Net: "udp"}
+	tcpServer := &dns.Server{Addr: fPort, Net: "tcp"}
+	glogger.Info.Println("started server on", config.Blackhole.DNSPort)
+
+	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
+		// reutrn nonexistent reply  to all requests
+		glogger.Debug.Printf("handling repose, sending rcode3")
+
+		hostname := req.Question[0].Name
+
+		rr := new(dns.A)
+		rr.Hdr = dns.RR_Header{Name: hostname, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 1}
+		rr.A = net.ParseIP("")
+
+		// craft reply
+		rep := new(dns.Msg)
+		rep.SetReply(req)
+		rep.SetRcode(req, dns.RcodeNameError)
+		rep.Answer = append(rep.Answer, rr)
+
+		// send it
+		w.WriteMsg(rep)
+
+		// add hostname to redis db
+		redisClient.SAdd("index:domains", hostname)
+	})
+
+	go func() {
+		glogger.Error.Println(udpServer.ListenAndServe())
+	}()
+	glogger.Error.Println(tcpServer.ListenAndServe())
+}
+
+func readConf() {
+	// init config file
+	err := gcfg.ReadFileInto(&config, "config.gcfg")
+	if err != nil {
+		panic(fmt.Sprintf("Could not load config.gcfg, error: %s\n", err))
+	}
+}
+
+func initLogger(logLevel string) {
+	// init logger
+	if logLevel == "debug" {
+		glogger.LogInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+	} else if logLevel == "cluster" {
+		glogger.LogInit(os.Stdout, os.Stdout, ioutil.Discard, os.Stderr)
+	} else if logLevel == "info" {
+		glogger.LogInit(os.Stdout, ioutil.Discard, ioutil.Discard, os.Stderr)
+	} else {
+		glogger.LogInit(ioutil.Discard, ioutil.Discard, ioutil.Discard, os.Stderr)
+	}
+}
+
+func initRedisConnection() (*redis.Client, error) {
+	// init redis connection
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     config.Redis.Host,
+		Password: config.Redis.Password,
+		DB:       0,
+	})
+
+	_, redisErr := redisClient.Ping().Result()
+	return redisClient, redisErr
+}
